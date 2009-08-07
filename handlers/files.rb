29668f1730b851579a2ad6ef754259e3996f4b7f
Miika Leppänen <milep@mlemacbook.local>
2009-08-07 11:32:04 +0300

    moved user loaded images to user_data folder

--- handlers/files.rb
+++ handlers/files.rb
@@ -2,7 +2,6 @@ get '/admin/upload' do
   show_header
   show_menu
   protected!
-  #@filename = ""
   erb :upload
 end
 
@@ -11,7 +10,7 @@ post '/admin/upload' do
   show_menu
   protected!
   tempfile = params[:file][:tempfile]
-  @filename = "/images/#{params[:file][:filename]}"
+  @filename = "/user_data/#{params[:file][:filename]}"
   FileUtils.copy(tempfile.path, "#{Dir.pwd}/public#{@filename}")
   FileUtils.chmod(0644, "#{Dir.pwd}/public#{@filename}")
   erb :upload
@@ -19,10 +18,12 @@ end
 
 get '/javascript/lists/image_list.js' do
   content_type("text/javascript")
-  dir = Dir.new(Dir.pwd + "/public/images")
+  dir = Dir.new(Dir.pwd + "/public/user_data")
   @files = []
   dir.each do |filename|
-    @files << "[\"#{filename}\", \"/images/#{filename}\"]" if File.file?(Dir.pwd + "/public/images/" + filename)
+    if filename =~ /png$|jpg$|jpeg$|gif$/
+      @files << "[\"#{filename}\", \"/user_data/#{filename}\"]" if File.file?(Dir.pwd + "/public/user_data/" + filename)
+    end
   end
   erb :"javascripts/image_list", :layout => false
 end
