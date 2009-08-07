07c678c6e925206084818babb7aa4406aa35134f
Miika Leppänen <milep@mlemacbook.local>
2009-08-07 10:10:21 +0300

    db creation script and config for smtp

---
+++ migrate.rb
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'dm-core'
+require 'lib/models/user'
+require 'lib/models/content'
+
+if ARGV.first == "production"
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/db/teenycms_prod.sqlite3")
+  DataMapper.auto_migrate!
+else
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/db/teenycms.sqlite3")
+  DataMapper.auto_migrate!
+end
